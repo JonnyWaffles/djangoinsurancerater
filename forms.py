facb9f83e4e509eb03d6237c8acb393e00865363
mr.waffles <mr.waffles@gmail.com>
2016-08-29 19:36:56 -0400

    Working on views

--- forms.py
+++ forms.py
@@ -1,9 +1,17 @@
-""" 
 from django.forms import ModelForm
-from djangoinsurancerater.models import AccountInfo
+from djangoinsurancerater.models import AccountInfo, RiskData, InsuringAgreement
 
 class AccountInfoForm(ModelForm):
   class Meta:
     model = AccountInfo
-    fields = ['name', 'rating_state', 'entity_type']
-"""
+    exclude = ['title']
+    
+class RiskDataForm(ModelForm):
+  class Meta:
+    model = RiskData
+    fields = '__all__'
+
+class InsuringAgreementForm(ModelForm):
+  class Meta:
+    model = InsuringAgreement
+    fields = ['agreement_type', 'insurance_limit', 'deductible' ]
--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.edit import CreateView, UpdateView 
 from django.views.generic.detail import DetailView 
+from django.views.generic import View
 from django.http import HttpResponseRedirect
-from models import AccountInfo, Quote, RiskData 
+from models import AccountInfo, Quote, RiskData
+from .forms import AccountInfoForm, RiskDataForm, InsuringAgreementForm
 
 # Create your views here.
 
@@ -16,12 +18,16 @@ class AccountUpdateView(UpdateView):
   fields = ['name', 'underwriter', 'rating_state', 'entity_type']
   template_name = "djangoinsurancerater/accountinfo_form.html"
 
-""" Psuedo Code Time
-
-class QuoteCreateView(CreateView):
-  model = Quote
-  fields = ['underwriter', 'created_time', 'account_info']
-
-"""
+class QuoteView(View):
+  def get(self, request, *args, **kwargs):
+    accountinfo = get_object_or_404(AccountInfo, pk=kwargs['pk'])
+    accountinfoform = AccountInfoForm(instance=accountinfo)
+    riskdataform = RiskDataForm()
+    context = {'accountinfoform' : accountinfoform, 'riskdataform' : riskdataform}
+    return render(request, 'djangoinsurancerater/quotes.html', context)
+    
+    
+    
+    
 
       
